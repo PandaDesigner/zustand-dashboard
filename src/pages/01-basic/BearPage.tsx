6c83f3382e5c0afd11240dd99bd95b6cbba72594
Pedro Fernandez <pedro@pandadesigners.com>
2025-01-25 22:18:28 -0500

    funcionality bears storage in zustand

--- src/pages/01-basic/BearPage.tsx
+++ src/pages/01-basic/BearPage.tsx
@@ -1,6 +1,9 @@
+import { useShallow } from 'zustand/shallow';
 import { WhiteCard } from '../../components';
+import { userBearStore } from '../../stores/bears';
 
 export const BearPage = () => {
+
   return (
     <>
       <h1>Contador de Osos</h1>
@@ -8,46 +11,92 @@ export const BearPage = () => {
       <hr />
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
-
-        <WhiteCard centered>
-          <h2>Osos Negros</h2>
-
-          <div className="flex flex-col md:flex-row">
-            <button> +1</button>
-            <span className="text-3xl mx-2 lg:mx-10"> 0 </span>
-            <button>-1</button>
-          </div>
-
-        </WhiteCard>
-
-        <WhiteCard centered>
-          <h2>Osos Polares</h2>
-
-          <div className="flex flex-col md:flex-row">
-            <button> +1</button>
-            <span className="text-3xl mx-2 lg:mx-10"> 0 </span>
-            <button>-1</button>
-
-          </div>
-
-        </WhiteCard>
-
-        <WhiteCard centered>
-          <h2>Osos Pandas</h2>
-
-          <div className="flex flex-col md:flex-row">
-            <button> +1</button>
-            <span className="text-3xl mx-2 lg:mx-10"> 0 </span>
-            <button>-1</button>
-          </div>
-
-        </WhiteCard>
-
-
-
-
+        <BearPage.BlackBears />
+        <BearPage.PolarBears />
+        <BearPage.PandaBears />
+        <BearPage.BearsDisplay />
       </div>
 
     </>
   );
 };
+
+
+export const BlackBears = () => {
+  const blackBears = userBearStore((state) => state.blackBears);
+  const increaBlackBears = userBearStore((state) => state.increaeBlackBears);
+  return (<WhiteCard centered>
+    <h2>Osos Negros</h2>
+    <div className="flex flex-col md:flex-row">
+      <button onClick={() => increaBlackBears(+1)}> +1</button>
+      <span className="text-3xl mx-2 lg:mx-10"> {blackBears} </span>
+      <button onClick={() => increaBlackBears(-1)}>-1</button>
+    </div>
+  </WhiteCard>)
+
+}
+
+export const PolarBears = () => {
+  const polarBears = userBearStore((state) => state.polarBears);
+  const increasePolarBears = userBearStore((state) => state.increasePolarBears);
+  return (<WhiteCard centered>
+    <h2>Osos Polar</h2>
+    <div className="flex flex-col md:flex-row">
+      <button onClick={() => increasePolarBears(+1)}> +1</button>
+      <span className="text-3xl mx-2 lg:mx-10"> {polarBears} </span>
+      <button onClick={() => increasePolarBears(-1)}>-1</button>
+    </div>
+  </WhiteCard>)
+}
+
+export const PandaBears = () => {
+  const pandaBears = userBearStore((state) => state.pandaBears);
+  const increasePandaBears = userBearStore((state) => state.increasePandaBears);
+  return (<WhiteCard centered>
+    <h2>Osos Panda</h2>
+    <div className="flex flex-col md:flex-row">
+      <button onClick={() => increasePandaBears(+1)}> +1</button>
+      <span className="text-3xl mx-2 lg:mx-10"> {pandaBears} </span>
+      <button onClick={() => increasePandaBears(-1)}>-1</button>
+    </div>
+  </WhiteCard>)
+}
+
+export const BearsDisplay = () => {
+
+  const bears = userBearStore(useShallow((state) => state.bears));
+  const addBear = userBearStore((state) => state.addBear);
+  const clearBears = userBearStore((state) => state.clearBears)
+
+  return (
+    <WhiteCard centered>
+      <h2>
+        Osos
+      </h2>
+      <div className='flex flex-row gap-2 my-4'>
+        <button className='text-xs px-4 py-2 my-0 font-light' onClick={() => addBear()} >Agregar Oso</button>
+        <button className='text-xs px-4 py-2 my-0 font-light' onClick={clearBears}>Eliminar Osos</button>
+      </div>
+      <ul>
+        {bears.map((bear) => (
+          <li key={bear.id} className='text-xs font-normal'>
+            <p className='font-semibold'>
+              Name Bears: <span className='font-light'>{bear.name}</span>
+            </p>
+            <span>
+              <span className='font-bold'>Type: </span> {bear.type}
+            </span>
+            <hr />
+          </li>
+        ))}
+      </ul>
+
+
+    </WhiteCard>
+  )
+};
+
+BearPage.BlackBears = BlackBears;
+BearPage.PolarBears = PolarBears;
+BearPage.PandaBears = PandaBears;
+BearPage.BearsDisplay = BearsDisplay;
--- src/stores/bears/bears.stora.ts
+++ src/stores/bears/bears.stora.ts
@@ -1,15 +1,56 @@
 import { create } from 'zustand';
 
+interface Bear {
+    id: number;
+    name: string;
+    type: string;
+}
+
+const typeBear = ['Black Bears', 'Polar Bears', 'Panda Bears']
 interface BearStore {
     blackBears: number;
     polarBears: number;
     pandaBears: number;
+    computed: {
+        totalBears: number
+    };
+
+
+    bears: Bear[];
+
+
+    increaeBlackBears: (by: number) => void;
+    increasePolarBears: (by: number) => void;
+    increasePandaBears: (by: number) => void;
+    removeAllBears: () => void;
+    addBear: () => void;
+    clearBears: () => void;
 }
 
-export const userBearStore = create<BearStore>()((set) => ({
+export const userBearStore = create<BearStore>()((set, get) => ({
     blackBears: 10,
     polarBears: 5,
     pandaBears: 1,
-    incrementPopulation: (by: number) => set((states) => ({ blackBears: states.blackBears + by })),
-    removeAllBears: () => set({ blackBears: 0, polarBears: 0, pandaBears: 0 })
+    bears: [{ id: 1, name: 'Black Bear', type: 'blackBears' }, { id: 2, name: 'Polar Bear', type: 'polarBears' }, { id: 3, name: 'Panda Bear', type: 'pandaBears' }],
+
+    computed: {
+        get totalBears() {
+            return get().blackBears + get().polarBears + get().pandaBears + get().bears.length
+        }
+    },
+
+    increaeBlackBears: (by: number) => set((states) => ({ blackBears: states.blackBears + by })),
+    increasePolarBears: (by: number) => set((states) => ({ polarBears: states.polarBears + by })),
+    increasePandaBears: (by: number) => set((states) => ({ pandaBears: states.pandaBears + by })),
+    removeAllBears: () => set({ blackBears: 0, polarBears: 0, pandaBears: 0 }),
+    addBear: () => {
+        const bear = typeBear[Math.floor(Math.random() * typeBear.length)];
+        const newBear = bear.split(' ');
+        set((state) => ({
+            bears: [
+                ...state.bears,
+                { id: state.bears.length + 1, name: 'New Bear ' + newBear[0], type: bear }]
+        }))
+    },
+    clearBears: () => set({ bears: [] })
 }))
